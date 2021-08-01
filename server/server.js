90e556d8175c0c91f6083c65138b851510f56d94
Lance Mac Donald <lance@saeon.ac.za>
2021-08-01 22:52:49 +0200

    Moved GraphQL schema and Root function out of `server.js`

--- server/server.js
+++ server/server.js
@@ -1,37 +1,18 @@
 var express = require('express');
-var client = require('./mongoClient');
 var { graphqlHTTP } = require('express-graphql');
-var { buildSchema } = require('graphql');
 var cors = require('cors');
+var schema = require('./db/schema');
+var root = require('./db/root');
+const { PATH, PORT } = require('./config/settings');
 
-// Construct a schema, using GraphQL schema language
-var schema = buildSchema(`type Query 
-{ 
-  Log: String, 
-  Log_Click: String,
-  Log_Download: String,
-  Log_MouseMove: String,
-  Log_Query: String
-}`);
-
-
-// The root provides a resolver function for each API endpoint
-var root = { 
-    Log: async () => { return await client.logPipeline(); },
-    Log_Click: async () => { return await client.logPipelineClick(); },
-    Log_Download: async () => { return await client.logPipelineDownload(); },
-    Log_MouseMove: async () => { return await client.logPipelineMouseMove(); },
-    Log_Query: async () => { return await client.logPipelineQuery(); }
-   };
- 
 var app = express();
 
 app.use(cors());
-app.use('/graphql', graphqlHTTP({
-  schema: schema,
-  rootValue: root,
+app.use(`/${PATH}`, graphqlHTTP({
+  schema: schema.graphql_schema,
+  rootValue: root.graphql_root,
   graphiql: true,
 }));
-app.listen(4000);
-console.log('[+] - Running a GraphQL API server at http://localhost:4000/graphql');
+app.listen(PORT);
+console.log(`[+] - Running a GraphQL API server at http://localhost:${PORT}/${PATH}`);
 
